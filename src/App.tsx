61d55150fd9e5293aa4df3e2dada60ea73f009b6
Thiago Nunes Miziara <thiagonmiziara@gmail.com>
2022-08-13 16:35:17 -0300

    montando a estrutura html do post

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 import "./global.css";
-import Post from "./components/Post";
-import { Sidebar } from "./components/Sidebar";
 
 export const App = () => {
   return (
@@ -14,16 +14,8 @@ export const App = () => {
         <Sidebar />
 
         <main>
-          <Post
-            author='Lorem Ipsum'
-            postMessage='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, deleniti cupiditate debitis laborum sequi blanditiis ea ratione quod error! Vel minima saepe voluptatum doloremque illum aut similique hic ipsam quo!
-'
-          />
-          <Post
-            author='Lorem Ipsum'
-            postMessage='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, deleniti cupiditate debitis laborum sequi blanditiis ea ratione quod error! Vel minima saepe voluptatum doloremque illum aut similique hic ipsam quo!
-'
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,15 +1,40 @@
-interface IPostProps {
-  author: string;
-  postMessage: string;
-}
+import styles from "./Post.module.css";
 
-const Post = ({ author, postMessage }: IPostProps) => {
+export const Post = () => {
   return (
-    <div>
-      <h4>{author}</h4>
-      <p>{postMessage}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src='https://avatars.githubusercontent.com/u/67029929?v=4'
+          />
+          <div className={styles.authorInfo}>
+            <strong>Thiago Nunes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='11 de maio às 08:00' dateTime='2022-05-11 8:00:00'>
+          Plicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nihil
+          reiciendis veniam architecto, nam nulla laborum laboriosam!
+        </p>
+
+        <p>
+          <a href='#'>thiago.developer/doctorcare</a>
+        </p>
+
+        <p>
+          <a href='#'>#novoprojeto #nlw #rocketseat</a>{" "}
+        </p>
+      </div>
+    </article>
   );
 };
-
-export default Post;
