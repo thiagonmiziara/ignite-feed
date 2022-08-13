57fdb3acb4883ba82d8904acf00addf86f3763e1
Thiago Nunes Miziara <thiagonmiziara@gmail.com>
2022-08-13 17:49:37 -0300

    criando estrutura de comentarios

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export const Comment = () => {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src='https://avatars.githubusercontent.com/u/67029929?v=4'
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Thiago Nunes</strong>
+              <time title='11 de maio às 08:00' dateTime='2022-05-11 8:00:00'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom parabéns!! 👏 👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,3 +1,4 @@
+import { Comment } from "../Comment";
 import styles from "./Post.module.css";
 
 export const Post = () => {
@@ -46,6 +47,12 @@ export const Post = () => {
           <button type='submit'>Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 };
