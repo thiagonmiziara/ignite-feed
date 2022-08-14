7030cbac73aa12e18476b69a26324c77f079d700
Thiago Nunes Miziara <thiagonmiziara@gmail.com>
2022-08-14 13:06:01 -0300

    add jest config prettier e new post

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,30 +1,31 @@
-import { ThumbsUp, Trash } from "phosphor-react";
-import { Avatar } from "../Avatar";
-import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from '../Avatar'
+import styles from './Comment.module.css'
 
-export const Comment = () => {
+interface ICommentProps {
+  content: string
+}
+
+export const Comment = ({ content }: ICommentProps) => {
   return (
     <div className={styles.comment}>
-      <Avatar
-        hasBorder={false}
-        src='https://avatars.githubusercontent.com/u/67029929?v=4'
-      />
+      <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/67029929?v=4" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Thiago Nunes</strong>
-              <time title='11 de maio às 08:00' dateTime='2022-05-11 8:00:00'>
+              <time title="11 de maio às 08:00" dateTime="2022-05-11 8:00:00">
                 Cerca de 1h atrás
               </time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom parabéns!! 👏 👏</p>
+          <p>{content} 👏 👏</p>
         </div>
 
         <footer>
@@ -35,5 +36,5 @@ export const Comment = () => {
         </footer>
       </div>
     </div>
-  );
-};
+  )
+}
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,44 +1,55 @@
-import {
-  formattedDateTitle,
-  publishedDateRelativeToNow,
-} from "../../utils/date";
-import { Avatar } from "../Avatar";
-import { Comment } from "../Comment";
-import styles from "./Post.module.css";
+import { useState } from 'react'
+import { formattedDateTitle, publishedDateRelativeToNow } from '../../utils/date'
+import { Avatar } from '../Avatar'
+import { Comment } from '../Comment'
+import styles from './Post.module.css'
 
 type TAuthor = {
-  avatarUrl: string;
-  name: string;
-  role: string;
-};
+  avatarUrl: string
+  name: string
+  role: string
+}
 
 type TContent = {
-  type: string;
-  content: string;
-};
+  type: string
+  content: string
+}
 
 interface IPostProps {
-  author: TAuthor;
-  publishedAt: Date;
-  content: TContent[];
+  author: TAuthor
+  publishedAt: Date
+  content: TContent[]
 }
 
 export const Post = ({ author, publishedAt, content }: IPostProps) => {
+  const [comments, setComments] = useState(['Primeiro comentário'])
+  const [newComment, setNewComment] = useState('')
+
   const contentInfo = (content: TContent[]) => {
-    return content.map((item) => {
+    return content.map(item => {
       switch (item.type) {
-        case "paragraph":
-          return <p key={item.content.length}>{item.content}</p>;
-        case "link":
+        case 'paragraph':
+          return <p key={item.content.length}>{item.content}</p>
+        case 'link':
           return (
             <p key={item.content.length}>
-              <a href='#'>{item.content}</a>
+              <a href="#">{item.content}</a>
             </p>
-          );
+          )
         default:
       }
-    });
-  };
+    })
+  }
+
+  const handleCreateNewComment = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setComments([...comments, newComment])
+    setNewComment('')
+  }
+
+  const newCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewComment(event.target.value)
+  }
 
   return (
     <article className={styles.post}>
@@ -51,31 +62,33 @@ export const Post = ({ author, publishedAt, content }: IPostProps) => {
           </div>
         </div>
 
-        <time
-          title={formattedDateTitle(publishedAt)}
-          dateTime={publishedAt.toISOString()}
-        >
+        <time title={formattedDateTitle(publishedAt)} dateTime={publishedAt.toISOString()}>
           {publishedDateRelativeToNow(publishedAt)}
         </time>
       </header>
 
       <div className={styles.content}>{contentInfo(content)}</div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={event => handleCreateNewComment(event)} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea name='comment' placeholder='Deixe seu comentário' />
+        <textarea
+          name="comment"
+          placeholder="Deixe seu comentário"
+          value={newComment}
+          onChange={event => newCommentChange(event)}
+        />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type="submit">Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => (
+          <Comment key={`${comment.length + 1}`} content={comment} />
+        ))}
       </div>
     </article>
-  );
-};
+  )
+}
